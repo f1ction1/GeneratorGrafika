94b6003b30c74156ae87889a36feaedbb1521185
f1ction1 <kazanzew13@gmail.com>
2025-10-14 23:17:35 +0200

    Added database configuration and ORM models

--- backend/user-service/app/main.py
+++ backend/user-service/app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from pydantic import BaseModel # for data validation
+from db import engine, Base
 
-app = FastAPI()
+app = FastAPI(title="User Service API")
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
