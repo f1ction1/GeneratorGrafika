8ac5c5af620b0b51fc99409e74373fedf688982b
f1ction1 <kazanzew13@gmail.com>
2025-10-27 21:26:20 +0100

    Add function to get user using JWT token

---
+++ backend/user-service/app/core/security.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from db import get_db
+from models.User import User
+import os
+from fastapi.security import OAuth2PasswordBearer
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-change-me")
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+
+
+def get_current_user(token: str= Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("id")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = db.query(User).filter(User.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+    return user
