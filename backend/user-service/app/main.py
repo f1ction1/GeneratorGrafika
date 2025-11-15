d2b3bf5ad97f19cb1b0e2c018423d184781276f4
Szymon Zieliński <Simon132445@gmail.com>
2025-11-15 16:33:16 +0100

    Added update user functionality

--- backend/user-service/app/main.py
+++ backend/user-service/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from db import engine, Base
 from api.routers import employer, auth
+from api.routers import user
 
 app = FastAPI(title="User Service API")
 Base.metadata.create_all(bind=engine)
@@ -8,6 +9,7 @@ Base.metadata.create_all(bind=engine)
 # register auth routes under /auth
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(employer.router)
+app.include_router(user.router)
 
 @app.get("/")
 def root():
